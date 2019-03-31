3a373926708112c41f932a404262c6f8236f1a46
Marcel Balint <blank_blankness@yahoo.com>
2019-03-31 15:21:16 +0000

    Adding drawing feature

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 // Create divs and append them to container.
 
+grid();
+
 function grid(n) {
-  
+
     let container = document.querySelector("#container");
     n = 16;
     for (let i = 0; i < n * n; i++) {
-      let content = document.createElement("div");
-      content.setAttribute("style","background-color: LightGray")
-      container.appendChild(content);
-      container.setAttribute("style", `grid: repeat(${n}, auto) / repeat(${n}, auto)`);
-  
+        let content = document.createElement("div");
+        content.classList.add("content");
+        content.setAttribute("style", "background-color: white");
+        container.appendChild(content);
+        container.setAttribute("style", `grid: repeat(${n}, auto) / repeat(${n}, auto)`);
+
     }
-  }
-  grid();
+}
+
+
+//black color
+function black() {
+
+    document.querySelectorAll(".content").forEach(
+        content =>
+        (content.onmouseover = () => {
+            content.setAttribute(
+                "style",
+                `background-color: #000`);
+        })
+    );
+}
+//white color
+function white() {
+
+    document.querySelectorAll(".content").forEach(
+        content =>
+        (content.onmouseover = () => {
+            content.setAttribute(
+                "style",
+                `background-color: #fff`);
+        })
+    );
+}
