9f0b6c1b340ce61be039730bfad1c2ffcf35664e
Marcel Balint <blank_blankness@yahoo.com>
2019-03-31 18:50:47 +0000

    Created a 'reset' button

--- script.js
+++ script.js
@@ -2,20 +2,25 @@
 
 grid();
 
-function grid(n) {
+function grid(n = 16) {
 
     let container = document.querySelector("#container");
-    n = 16;
+  
     for (let i = 0; i < n * n; i++) {
         let content = document.createElement("div");
         content.classList.add("content");
-        content.setAttribute("style", "background-color: white");
+        content.setAttribute("style", "background-color: #fff");
         container.appendChild(content);
         container.setAttribute("style", `grid: repeat(${n}, auto) / repeat(${n}, auto)`);
-
+        
     }
+    black();
 }
-
+function reset() {
+  document.querySelector("#container").innerHTML = "";
+  let ask = prompt("How many squares? (Enter a number between 2 and 70)",16);
+  grid(ask);
+}
 
 //black color
 function black() {
